be6e55c746f0e1d072eb7468ced3748fb204294f
Archchaya Uthayakumar <uthyakumararchchaya@gmail.com>
2022-03-01 19:17:00 +0530

    result.js updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { Input } from '@material-ui/core';
-import InputBox from './components/InputBox';
+import React, { useState } from "react";
+import InputBox from "./components/InputBox";
+import axios from "axios";
+import Result from "./components/Result";
 
 
-const App =() => {
+const App = () => {
+  const [input, setInput] = useState("");
+  const [desc, setDesc] = useState("");
+  const [temp, setTemp] = useState("");
+  const [icon, setIcon] = useState("");
+
+  const findWeather = async() => {
+    const apiKey = "eae2d7f39584a05c13615266e197ed9b";
+    const unit = "metric";
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      input +
+      "&appid=" +
+      apiKey +
+      "&units=" +
+      unit;
+    const response = await axios.get(url);
+    const temp = await response.data.main.temp;
+    const weatherDesc = await response.data.weather[0].description;
+    const icon = await response.data.weather[0].icon;
+    const imageURL =
+      "https://favpng.com/png_view/shower-weather-icon-material-weather-forecasting-rain-icon-png/SQ1VhtZn" +
+      icon +
+      "@2x.png";
+    setTemp(temp);
+    setDesc(weatherDesc);
+    setIcon(imageURL);
+    setInput("");
+  };
+
   return (
-    <div className="App">
-      <InputBox />
-     <Input />
+    <div>
+      {temp === "" ? (
+        <InputBox setInput={setInput} input={input} findWeather={findWeather} />
+      ) : (
+        <Result temp={temp} desc={desc} image={icon} setTemp={setTemp} />
+
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,28 +1,38 @@
-import { Button, Input } from '@material-ui/core';
-import React from 'react';
+import { Button, Input } from "@material-ui/core";
+import React from "react";
+import "./InputBox.css";
 
+const InputBox = ({ input, setInput, findWeather }) => {
+    const functionCall = (event) => {
+        event.preventDefault();
+        findWeather();
+    }
+  return (
+    <div className="inputBox">
+      <div className="inputBox_field">
+        <h1>Google Weather App</h1>
+        <p>Powered By</p>
 
-const InputBox=() => {
-    return (
-        <div className='inputBox'>
-            <div className = 'inputBox_field'>
-                <h1>Google Weather App</h1>
-                <p>Powered By</p>
+        <img
+          src="https://clipart.world/wp-content/uploads/2020/10/Google-Logo-clipart-transparent.png"
+          alt="logo"
+        />
 
-                <img src ="https://clipart.world/wp-content/uploads/2020/10/Google-Logo-clipart-transparent.png" 
-                alt="logo" />
+        <form>
+          <Input
+            placeholder="Enter the city name"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+          <br />
+                  <Button variant="contained" color="secondary" type="submit"
+                      onClick={functionCall}>
+            Search
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-                <form>
-                    <Input placeholder="Enter the city name" />
-                    <br />
-                    <Button variant="contained" color="secondary" type="submit">
-                        Search
-                    </Button>
-                </form>
-            </div>
-            
-        </div>
-    )
-}
-
-export default InputBox
+export default InputBox;
